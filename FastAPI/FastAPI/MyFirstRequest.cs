0acc96a79803dd9cbb8ddb0e1859020c2a2b9923
Rod Falanga <Rod-in-NM@users.noreply.github.com>
2024-08-18 09:38:50 -0600

    Added a couple DTO classes

---
+++ FastAPI/FastAPI/MyFirstRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace FastAPI
+{
+    // DTO
+    public class MyFirstRequest
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
---
+++ FastAPI/FastAPI/MyFirstResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace FastAPI
+{
+    // Another DTO
+    public class MyFirstResponse
+    {
+        public string Message { get; set; } = string.Empty;
+    }
+}
