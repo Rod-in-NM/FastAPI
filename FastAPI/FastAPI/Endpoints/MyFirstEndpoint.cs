62a648ea8e7a5ca34d4c1d854077c12796801ec1
Rod Falanga <Rod-in-NM@users.noreply.github.com>
2024-08-18 10:43:19 -0600

    Added endpoint class #2

---
+++ FastAPI/FastAPI/Endpoints/MyFirstEndpoint.cs
@@ -0,0 +1,23 @@
+﻿using FastEndpoints;
+
+namespace FastAPI.Endpoints
+{
+    public class MyFirstEndpoint : Endpoint<MyFirstRequest, MyFirstResponse>
+    {
+        // How the endpoint should listen to incoming requests
+        public override void Configure()
+        {
+            Post("/api/author/create");
+            AllowAnonymous();
+        }
+
+        // Logic for processing the request and send back the response
+        public override async Task HandleAsync(MyFirstRequest request, CancellationToken token)
+        {
+            await SendAsync(new()
+            {
+                Message = $"The author name is {request.FirstName} {request.LastName}"
+            });
+        }
+    }
+}
